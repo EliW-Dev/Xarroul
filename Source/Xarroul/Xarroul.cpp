cdf4b3473b196990a525879e39506c1f2f4db150
EliWhitbread <eli.whitbread@gmail.com>
2022-01-13 17:32:01 +1100

    Initial project push. updated gitignore
    Updated gitignore to omit StarterContent and RiderLink
    New UE5 project
    Created new MainGame level
    Added 3 models for background and obstacle assets

---
+++ Source/Xarroul/Xarroul.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Xarroul.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Xarroul, "Xarroul" );
---
+++ Source/Xarroul/XarroulGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "XarroulGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XARROUL_API AXarroulGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
